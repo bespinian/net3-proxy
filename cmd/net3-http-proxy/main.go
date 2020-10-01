f34fbaa09baaecfb9ed14b074c7d78d862ffc5bb
Mathis Kretz <mathis@bespinian.io>
2020-10-01 22:24:29 +0200

    Make target protocol configurable

--- cmd/net3-http-proxy/main.go
+++ cmd/net3-http-proxy/main.go
@@ -17,13 +17,14 @@ func main() {
 	log.Printf("Starting proxy.")
 
 	net3HttpProxyPort := getEnv("NET3_HTTP_PROXY_PORT", "81")
+	net3HttpProxyTargetProtocol := getEnv("NET3_HTTP_PROXY_TARGET_PROTOCOL", "http")
 	net3HttpProxyTargetHost := getEnv("NET3_HTTP_PROXY_TARGET_HOST", "127.0.0.1")
 	net3HttpProxyTargetPort := getEnv("NET3_HTTP_PROXY_TARGET_PORT", "80")
 
 	log.Printf("Listening on port %q", net3HttpProxyPort)
 	log.Printf("Forwarding to host %q on port %q", net3HttpProxyTargetHost, net3HttpProxyTargetPort)
 
-	http.HandleFunc("/", getRequestAndForwardHandler(net3HttpProxyTargetHost, net3HttpProxyTargetPort))
+	http.HandleFunc("/", getRequestAndForwardHandler(net3HttpProxyTargetProtocol, net3HttpProxyTargetHost, net3HttpProxyTargetPort))
 	if err := http.ListenAndServe(":"+net3HttpProxyPort, nil); err != nil {
 		log.SetOutput(os.Stderr)
 		log.Print(fmt.Errorf("Could not start proxy server: %w", err))
@@ -40,7 +41,7 @@ func getEnv(key, fallback string) string {
 	return fallback
 }
 
-func getRequestAndForwardHandler(targetHost, targetPort string) func(res http.ResponseWriter, req *http.Request) {
+func getRequestAndForwardHandler(targetProtocol, targetHost, targetPort string) func(res http.ResponseWriter, req *http.Request) {
 	return func(res http.ResponseWriter, req *http.Request) {
 
 		body, err := ioutil.ReadAll(req.Body)
@@ -55,7 +56,7 @@ func getRequestAndForwardHandler(targetHost, targetPort string) func(res http.Re
 		log.Printf("Request body: %q", body)
 
 		req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-		targetUrl, err := url.Parse(targetHost + ":" + targetPort)
+		targetUrl, err := url.Parse(targetProtocol + "://" + targetHost + ":" + targetPort)
 		if err != nil {
 			log.SetOutput(os.Stderr)
 			log.Print(fmt.Errorf("invalid proxy target url: %w", err))
