e848fd632a9cc4666e59f7a0051ab9f25a1a9fbb
Mathis Kretz <mathis@bespinian.io>
2020-10-09 14:49:07 +0200

    Improve body handling, remove target protocol

--- cmd/net3-http-proxy/main.go
+++ cmd/net3-http-proxy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -19,7 +20,6 @@ func main() {
 	if err != nil {
 		log.Fatal(fmt.Errorf("Invalid value for port: %w", err))
 	}
-	targetProtocol := getEnv("NET3_HTTP_PROXY_TARGET_PROTOCOL", "http")
 	targetHost := getEnv("NET3_HTTP_PROXY_TARGET_HOST", "localhost")
 	targetPortStr := getEnv("NET3_HTTP_PROXY_TARGET_PORT", "80")
 	targetPort, err := strconv.Atoi(targetPortStr)
@@ -30,7 +30,7 @@ func main() {
 	log.Printf("Listening on port %v", port)
 	log.Printf("Forwarding to host %q on port %v", targetHost, targetPort)
 
-	http.HandleFunc("/", makeProxyHandleFunc(targetProtocol, targetHost, targetPort))
+	http.HandleFunc("/", makeProxyHandleFunc(targetHost, targetPort))
 	err = http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
 	if err != nil {
 		log.Fatal(fmt.Errorf("Could not start proxy server: %w", err))
@@ -46,14 +46,9 @@ func getEnv(key, fallback string) string {
 	return value
 }
 
-func makeProxyHandleFunc(targetProtocol, targetHost string, targetPort int) func(res http.ResponseWriter, req *http.Request) {
+func makeProxyHandleFunc(targetHost string, targetPort int) func(res http.ResponseWriter, req *http.Request) {
 	return func(res http.ResponseWriter, req *http.Request) {
-		bodyCopy, err := req.GetBody()
-		if err != nil {
-			log.Print(fmt.Errorf("error getting request body: %w", err))
-		}
-
-		body, err := ioutil.ReadAll(bodyCopy)
+		body, err := ioutil.ReadAll(req.Body)
 		if err != nil {
 			log.Print(fmt.Errorf("error reading request body: %w", err))
 		}
@@ -66,9 +61,12 @@ func makeProxyHandleFunc(targetProtocol, targetHost string, targetPort int) func
 		}
 
 		log.Println("Request body:")
-		log.Println(body)
+		log.Println(string(body))
 
-		targetUrl, err := url.Parse(fmt.Sprintf("%s://%s:%v", targetProtocol, targetHost, targetPort))
+		req.Body.Close()
+		req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+		targetUrl, err := url.Parse(fmt.Sprintf("http://%s:%v", targetHost, targetPort))
 		if err != nil {
 			log.Print(fmt.Errorf("Invalid proxy target url: %w", err))
 		}
